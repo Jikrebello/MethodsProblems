58297ff7675723c50be885f60de6e745bd8823c1
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:36:12 +0200

    Add files via upload

---
+++ MethodProblems/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace MethodProblems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnVolume = new System.Windows.Forms.Button();
+            this.BtnEvenOrOdd = new System.Windows.Forms.Button();
+            this.BtnRemainder = new System.Windows.Forms.Button();
+            this.BtnCubeRoot = new System.Windows.Forms.Button();
+            this.BtnMailCostCalculator = new System.Windows.Forms.Button();
+            this.BtnDiceRoll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnVolume
+            // 
+            this.BtnVolume.Location = new System.Drawing.Point(12, 12);
+            this.BtnVolume.Name = "BtnVolume";
+            this.BtnVolume.Size = new System.Drawing.Size(204, 45);
+            this.BtnVolume.TabIndex = 0;
+            this.BtnVolume.Text = "Volume of a Box";
+            this.BtnVolume.UseVisualStyleBackColor = true;
+            this.BtnVolume.Click += new System.EventHandler(this.BtnVolume_Click);
+            // 
+            // BtnEvenOrOdd
+            // 
+            this.BtnEvenOrOdd.Location = new System.Drawing.Point(12, 63);
+            this.BtnEvenOrOdd.Name = "BtnEvenOrOdd";
+            this.BtnEvenOrOdd.Size = new System.Drawing.Size(204, 45);
+            this.BtnEvenOrOdd.TabIndex = 1;
+            this.BtnEvenOrOdd.Text = "Even Or Odd";
+            this.BtnEvenOrOdd.UseVisualStyleBackColor = true;
+            this.BtnEvenOrOdd.Click += new System.EventHandler(this.BtnEvenOrOdd_Click);
+            // 
+            // BtnRemainder
+            // 
+            this.BtnRemainder.Location = new System.Drawing.Point(12, 114);
+            this.BtnRemainder.Name = "BtnRemainder";
+            this.BtnRemainder.Size = new System.Drawing.Size(204, 45);
+            this.BtnRemainder.TabIndex = 2;
+            this.BtnRemainder.Text = "Remainder Calculator";
+            this.BtnRemainder.UseVisualStyleBackColor = true;
+            this.BtnRemainder.Click += new System.EventHandler(this.BtnRemainder_Click);
+            // 
+            // BtnCubeRoot
+            // 
+            this.BtnCubeRoot.Location = new System.Drawing.Point(12, 165);
+            this.BtnCubeRoot.Name = "BtnCubeRoot";
+            this.BtnCubeRoot.Size = new System.Drawing.Size(204, 45);
+            this.BtnCubeRoot.TabIndex = 3;
+            this.BtnCubeRoot.Text = "Cube Root Calculator";
+            this.BtnCubeRoot.UseVisualStyleBackColor = true;
+            this.BtnCubeRoot.Click += new System.EventHandler(this.BtnCubeRoot_Click);
+            // 
+            // BtnMailCostCalculator
+            // 
+            this.BtnMailCostCalculator.Location = new System.Drawing.Point(12, 216);
+            this.BtnMailCostCalculator.Name = "BtnMailCostCalculator";
+            this.BtnMailCostCalculator.Size = new System.Drawing.Size(204, 45);
+            this.BtnMailCostCalculator.TabIndex = 4;
+            this.BtnMailCostCalculator.Text = "Mail Cost Calculator";
+            this.BtnMailCostCalculator.UseVisualStyleBackColor = true;
+            this.BtnMailCostCalculator.Click += new System.EventHandler(this.BtnMailCostCalculator_Click);
+            // 
+            // BtnDiceRoll
+            // 
+            this.BtnDiceRoll.Location = new System.Drawing.Point(12, 267);
+            this.BtnDiceRoll.Name = "BtnDiceRoll";
+            this.BtnDiceRoll.Size = new System.Drawing.Size(204, 45);
+            this.BtnDiceRoll.TabIndex = 5;
+            this.BtnDiceRoll.Text = "Dice Roll";
+            this.BtnDiceRoll.UseVisualStyleBackColor = true;
+            this.BtnDiceRoll.Click += new System.EventHandler(this.BtnDiceRoll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 321);
+            this.Controls.Add(this.BtnDiceRoll);
+            this.Controls.Add(this.BtnMailCostCalculator);
+            this.Controls.Add(this.BtnCubeRoot);
+            this.Controls.Add(this.BtnRemainder);
+            this.Controls.Add(this.BtnEvenOrOdd);
+            this.Controls.Add(this.BtnVolume);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnVolume;
+        private System.Windows.Forms.Button BtnEvenOrOdd;
+        private System.Windows.Forms.Button BtnRemainder;
+        private System.Windows.Forms.Button BtnCubeRoot;
+        private System.Windows.Forms.Button BtnMailCostCalculator;
+        private System.Windows.Forms.Button BtnDiceRoll;
+    }
+}
+
---
+++ MethodProblems/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Globalization;
+
+namespace MethodProblems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * 1. Design a method to calculate the volume of a box when the length, width and height of
+         * the box are passed to the function.
+         */
+        private void BtnVolume_Click(object sender, EventArgs e)
+        {
+            float length = float.Parse(Interaction.InputBox("Enter in Length", "Volume Calculator", ""), CultureInfo.InvariantCulture);
+            float width = float.Parse(Interaction.InputBox("Enter in Width", "Volume Calculator", ""), CultureInfo.InvariantCulture);
+            float height = float.Parse(Interaction.InputBox("Enter in Height", "Volume Calculator", ""), CultureInfo.InvariantCulture);
+
+            float answer = Volume(length, width, height);
+
+            MessageBox.Show("The Volume of " + length.ToString() + ", " + width.ToString() + " and " + height.ToString() + " is " + answer.ToString());
+
+        }
+
+        private float Volume(float _x, float _y, float _z)
+        {
+            return _x * _y * _z;
+        }
+
+        /*
+         * 2. Design a method that will determine whether a number passed to it is even or odd. Eg.
+         * EvenOdd(15) should return the word ‘odd’ .
+         */
+        private void BtnEvenOrOdd_Click(object sender, EventArgs e)
+        {
+            float number = float.Parse(Interaction.InputBox("Enter in a number", "Even or Odd Calculator", ""), CultureInfo.InvariantCulture);
+            
+            MessageBox.Show(EvenOdd(number));
+
+        }
+
+        private string EvenOdd(float _number)
+        {
+            if (_number % 2 == 0)
+            {
+                return "This number is even";
+            }
+            else
+            {
+                return "This number is odd";
+            }
+        }
+
+        /*
+         * 3. Design a method that will determine the remainder of two numbers. 
+         * Eg. Remainder(12,7) should return the number 5. Note: The first number passed is always larger than second.
+         */
+        private void BtnRemainder_Click(object sender, EventArgs e)
+        {
+            float number1 = float.Parse(Interaction.InputBox("Enter in a number", "Remainder Calculator", ""), CultureInfo.InvariantCulture);
+            float number2 = float.Parse(Interaction.InputBox("Enter in a number", "Remainder Calculator", ""), CultureInfo.InvariantCulture);
+
+            MessageBox.Show("The Remainder of " + number1 + " and " + number2 + " is " + Remainder(number1, number2).ToString());
+        }
+
+        private float Remainder(float _x, float _y)
+        {
+            return _x % _y;
+        }
+
+        /*
+         * 4. Design a method that will determine the cube root of a number passed to it. Hint: C# has
+         * the a square root method but does not have a built in cube root method. You will have to
+         * remember some math. Does fractional exponents ring a bell?
+         */
+        private void BtnCubeRoot_Click(object sender, EventArgs e)
+        {
+            double number = float.Parse(Interaction.InputBox("Enter in a number", "Cube Root Calculator", ""), CultureInfo.InvariantCulture);
+
+            MessageBox.Show("The Cube root of " + number + " is " + CubeRoot(number).ToString());
+        }
+
+        private double CubeRoot(double _number)
+        {
+            return Math.Round(Math.Pow(_number, (double)1 / 3), 2);
+        }
+
+        /*
+         * 5. Design a function to determine the cost of mailing a package. Your function should
+         * expect the weight of the package to be passed as a parameter. Using this weight will
+         * permit the function to return a value that will reflect the cost of the mailing. You must 
+         * the following to determine the cost.
+         * Packages weighing less than 8 grams will cost .32
+         * Packages weighing more than 8 grams and less than or equal to 12 will cost .47
+         * Packages weighing more than 12 grams and less than or equal to 16 will cost .62
+         * Packages weighing more than 16 grams and less than or equal to 20 will cost .77
+         * Packages weighing more than 20 grams and less than or equal to 24 will cost .92
+         * Packages weighing more than 24 -> Display message “too heavy” and cost 0
+         */
+        private void BtnMailCostCalculator_Click(object sender, EventArgs e)
+        {
+            float weight = float.Parse(Interaction.InputBox("Enter in the Weight of the Package", "Package Calculator", ""), CultureInfo.InvariantCulture);
+
+            if (weight >= 24)
+                MessageBox.Show("Too heavy, must be 24 grams or under.");
+
+                MessageBox.Show("The price for a package weighing " + weight + " grams is $" + CalculatePrice(weight).ToString());
+        }
+
+        private float CalculatePrice(float _weight)
+        {
+            if (_weight <= 8)
+                return 0.32f;
+            else if (_weight > 8 && _weight <= 12)
+                return 0.47f;
+            else if (_weight > 12 && _weight <= 16)
+                return 0.62f;
+            else if (_weight > 16 && _weight <= 20)
+                return 0.77f;
+            else if (_weight > 20 && _weight <= 24)
+                return 0.92f;
+            else
+                return 0f;
+        }
+
+        /*
+         * Make a program that rolls dice  
+         */
+        private void BtnDiceRoll_Click(object sender, EventArgs e)
+        {
+            Random rng1 = new Random();
+            Random rng2 = new Random();
+            int winnings = 5;
+
+            while (true)
+            {
+                MessageBox.Show("You have " + winnings + " credits left");
+                string input = Interaction.InputBox(@"Do you want to roll the die?
+y/n.", "Dice roll game", "");
+
+                if (input != "y")
+                {
+                    MessageBox.Show("Thank you for playing.");
+                    break;
+                }
+
+                int sum = rng1.Next(1, 7) + rng2.Next(1, 7);
+
+                if (sum == 2 || sum == 12)
+                {
+                    winnings += 1;
+                    MessageBox.Show("You rolled a total of " + sum + ". You win! You have " + winnings + " credits." );
+                }
+                else if (sum != 7)
+                {
+                    MessageBox.Show("You rolled a total of " + sum + ". You break even! You have " + winnings + " credits.");
+                }
+                else
+                {
+                    winnings -= 2;
+                    MessageBox.Show("You rolled a total of " + sum + ". You break lose! You have " + winnings + " credits.");
+                    if (winnings <= 0)
+                    {
+                        MessageBox.Show("You lost all your credits, you lose.");
+                        break;
+                    }
+                }
+            }
+
+        }
+
+    }
+}
